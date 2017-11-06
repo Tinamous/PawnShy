29bd0b762157e06c2f07045ce96bbf18c73c319f
Stephen Harrison <Stephen.Harrison@AnalysisUK.com>
2017-11-06 11:27:37 +0000

    improved domain lookup / led status.

--- src/app.py
+++ src/app.py
@@ -32,9 +32,9 @@ class App():
 				self.do_card_id_lookup(uid)
 			else:
 				if isinstance(ndef_record, URIRecord): # - website. (if starts with http or https. Also check email:
-					self.do_web_lookup(uid, ndef_record)
+					self.do_ndef_web_lookup(uid, ndef_record)
 				elif isinstance(ndef_record, MIMERecord):
-					self.do_email_lookup(uid, ndef_record)
+					self.do_ndef_email_lookup(uid, ndef_record)
 				else:
 					print("Don't know how to handle ndef record. reverting to card id.")
 					self.do_card_id_lookup(uid)
@@ -45,14 +45,18 @@ class App():
 
 	# Expect ndef to be MIMERecord
 	# and the MIMERecord to be a vcard
-	def do_email_lookup(self, uid, ndef_record):
+	def do_ndef_email_lookup(self, uid, ndef_record):
 		print("Email lookup. Mime type:")
 		print (ndef_record.mimeType)
 		print("Email lookup. Data:")
 		print (ndef_record.data)
 
+		self.do_email_lookup("Test@domain.com")
+
+	def do_email_lookup(self, email):
+
 		self.led_driver.animate_whilst_hibp_lookup()
-		count = self.hibp_lookup.lookup_email("Test@domain.com")
+		count = self.hibp_lookup.lookup_email(email)
 
 		# Insert artificial delay to make it look pretty...
 		time.sleep(5)
@@ -61,7 +65,7 @@ class App():
 		self.led_driver.show_result_email_count(count)
 
 	# Expect URIRecord
-	def do_web_lookup(self, uid, ndef_record):
+	def do_ndef_web_lookup(self, uid, ndef_record):
 		print("Web address lookup")
 		uri = ndef_record.uri
 		print(uri)
@@ -69,14 +73,19 @@ class App():
 		print("Extract domain")
 		parsed_uri = urlparse(uri)
 		domain = '{uri.netloc}'.format(uri=parsed_uri)
-		print domain
+		print ("'" + domain + "'")
 
 		# Bodge warning!
 		# TODO: If uri starts with "http://mobile.board.net" it's the Signal NFC
 		# namebadge. Use card id lookup instead
-		if domain == "mobile.board.net":
+		if domain == "mobile.bcard.net":
 			return self.do_card_id_lookup(uid)
 
+		self.do_web_lookup(domain)
+
+	def do_web_lookup(self, domain):
+
+		self.led_driver.animate_whilst_hibp_lookup()
 		count = self.hibp_lookup.lookup_domain(domain)
 
 		# Insert artificial delay to make it look pretty...
@@ -94,13 +103,10 @@ class App():
 		# Or for non-programmable cards, use the id for domain lookup.
 		if card_id == "0430ACD2865880":
 			print("Steves Signal card")
-			count = self.hibp_lookup.lookup_email("Stephen.Harrison@AnalysisUK.com")
-			time.sleep(5)
-			self.led_driver.show_result_email_count(count)
+			self.do_email_lookup("Stephen.Harrison@AnalysisUK.com")
 		elif card_id == "62EB23EE":
 			print("Tesco.com blank rfid card")
-			count = self.hibp_lookup.lookup_domain("Tesco.com")
-			self.led_driver.show_result_web_count(count)
+			self.do_web_lookup("Tesco.com")
 		else:
 			print("Unknown card")
 
